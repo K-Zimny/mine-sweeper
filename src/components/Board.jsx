4f261ac3fbf7fb7300bc2c15a509e51ef43ce507
Ken Zimny <71086300+K-Zimny@users.noreply.github.com>
2024-08-08 13:18:22 +0000

    Handles mine / board logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Cell from "./Cell";
 
 const INITIAL_GAME_STATE = [
@@ -14,35 +14,62 @@ const INITIAL_GAME_STATE = [
 
 export default function Board() {
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+  const [isOver, setIsOver] = useState(false);
 
   // Map over rows and cells, updating the selected cell.
   const handleGameUpdate = (r, c) => {
     setGameState((prevState) => {
       return prevState.map((row, rowIndex) =>
         rowIndex == r
-          ? row.map((cell, cellIndex) => (cellIndex == c ? (cell = 1) : cell))
+          ? row.map((cell, cellIndex) => (cellIndex == c ? (cell = 2) : cell))
           : row
       );
     });
+    setIsOver(true);
   };
 
+  const handleNewGame = () => {
+    if (isOver) {
+      setGameState(INITIAL_GAME_STATE);
+      setIsOver(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isOver) {
+      setGameState((prevState) => {
+        return prevState.map((row) =>
+          row.map((cell) => (cell === 0 ? 1 : cell))
+        );
+      });
+    }
+  }, [isOver]);
+
   return (
-    <table className="board">
-      <tbody>
-        {/*Map over initial game state, generating all rows and cells*/}
-        {INITIAL_GAME_STATE.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {row.map((cell, cellIndex) => (
-              <Cell
-                key={[rowIndex, cellIndex]}
-                id={[rowIndex, cellIndex]}
-                gameState={gameState}
-                onGameUpdate={() => handleGameUpdate(rowIndex, cellIndex)}
-              />
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <table className="board">
+        <tbody>
+          {/*Map over initial game state, generating all rows and cells*/}
+          {INITIAL_GAME_STATE.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <Cell
+                  key={[rowIndex, cellIndex]}
+                  id={[rowIndex, cellIndex]}
+                  gameState={gameState}
+                  onGameUpdate={() => handleGameUpdate(rowIndex, cellIndex)}
+                />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {isOver && (
+        <>
+          <h2>Wow, you're bad at this.</h2>
+          <button onClick={handleNewGame}>Play Again?</button>
+        </>
+      )}
+    </>
   );
 }
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,10 +2,16 @@ export default function Cell({ onGameUpdate, gameState, id }) {
   return (
     <td
       id={id}
-      className={gameState[id[0]][id[1]] === 1 ? "active" : ""}
+      className={
+        gameState[id[0]][id[1]] === 1
+          ? "empty"
+          : gameState[id[0]][id[1]] === 2
+          ? "mine"
+          : ""
+      }
       onClick={onGameUpdate}
     >
-      {gameState[id[0]][id[1]] === 1 ? "X" : "  "}
+      {gameState[id[0]][id[1]] === 2 ? "X" : "  "}
     </td>
   );
 }
